8ce9f186052b835b561251ec57c1d9509e3893c1
tom_pro <tom_pro@maomao.com>
2019-01-02 15:48:54 +0800

    第三个步骤，完成图片墙

--- app/src/main/java/com/myjingdong001/HomeFragment.java
+++ app/src/main/java/com/myjingdong001/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.myjingdong001;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,7 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.myjingdong001.util.MyImageLoader;
 import com.youth.banner.Banner;
@@ -45,8 +51,32 @@ public class HomeFragment extends Fragment{
        // TextView contentTv = rootView.findViewById(R.id.content_tv);
        // contentTv.setText(mContentText);
         initBanner(homeView);
+        //5实现图片墙
+        initGalleryJinqiu(homeView);
+
         return homeView;
     }
+    //初始化 金秋风暴图片墙 的方法及参数
+    private Gallery jingqiu_gallery;//金秋风暴图片墙
+    private ImageAdapter imageAdapter;//图片适配器
+
+    public void initGalleryJinqiu(View homeView){
+        jingqiu_gallery = homeView.findViewById(R.id.index_jingqiu_gallery);
+        imageAdapter = new ImageAdapter(homeView.getContext());
+        jingqiu_gallery.setAdapter(imageAdapter);
+        //相应的点击事件
+        jingqiu_gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(adapterView.getContext(), "您点击的是" + i, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+
+
 //4实现广告轮播
     private void initBanner(View homeView) {
         List images = new ArrayList();//创建图片结合，并存放图片
@@ -63,5 +93,41 @@ public class HomeFragment extends Fragment{
         banner.setImages(images);//设置图片结合
         banner.start();
     }
+    //5
+    private class ImageAdapter extends BaseAdapter{
+        private Context context;
+        int mGralleyItemBackground;
+        int[] images = {R.drawable.index_gallery_01,R.drawable.index_gallery_02,R.drawable.index_gallery_03,R.drawable.index_gallery_04,R.drawable.index_gallery_05};
+        public ImageAdapter (Context context){
+            this.context = context;
+        }
 
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //在此最好判断一下view是否为空
+            ImageView image = new ImageView(context);
+            image.setImageResource(images[position]);
+            image.setAdjustViewBounds(true);
+
+            //设置宽高
+           /*image.setLayoutParams(new Gallery.LayoutParams(
+                   ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));*/
+            return image;
+        }
+    }
 }
